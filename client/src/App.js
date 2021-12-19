bee26870da1bb045843541b04cb614e80bd30aa8
cdh616x <91150236+cdh616x@users.noreply.github.com>
2021-12-19 18:18:09 -0500

    add client and server files to MERN
    configured client and server with React and Express;
    imported all middleware successfully;
    when npm start is run on client CLI; wireframe, bare website comes up (works!)

---
+++ client/src/App.js
@@ -0,0 +1,34 @@
+import React from "react";
+import { Container, AppBar, Typography, Grow, Grid } from "@material-ui/core";
+import Posts from ".//components/form/form.js";
+import Form from ".//components/posts/posts.js";
+import useStyles from "./styles"
+
+function App() {
+  const classes = useStyles();
+  return (
+    <>
+    <Container maxWidth="lg">
+     <AppBar className={classes.appBar} position="static" color="inherit">
+      <Typography variant="h2" align="center">Memories</Typography>
+     </AppBar>
+     <Grow in>
+      <Container>
+       <Grid container justify="space-between" alignItems="stretch" spacing={3}>
+        <Grid item xs={12} sm={7}>
+         <Posts />
+        </Grid>
+        <Grid item xs={12} sm={7}>
+         <Form />
+        </Grid>
+       </Grid>
+      </Container>
+      </Grow>
+     <h5 align="center">Created by Collin Hooper</h5>
+    </Container>
+    </>
+
+  );
+}
+
+export default App;
---
+++ server/index.js
@@ -0,0 +1,26 @@
+//jshint esversion:6
+
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import cors from "cors";
+
+import postRoutes from "./routes/posts.js";
+
+const app = express();
+
+app.use("/posts", postRoutes);
+
+//setup
+
+app.use(bodyParser.json({ limit: "30mb", extended:true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended:true }));
+app.use(cors());
+
+const CONNECTION_URL = "mongodb+srv://cdh616x:Lyr1c%40%40%40@cluster0.sstqy.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const PORT = process.env.PORT || 5000;
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true})//use for connecting to Mongo Atlas database
+  .then(() => app.listen(PORT, () => console.log(`Server running on port ${PORT}!`)))
+  .catch((error) => console.log(error.message));
+
+//mongoose.set({useFindAndModify: false});
